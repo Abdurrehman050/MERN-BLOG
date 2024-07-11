b3c751e10d18d3fc5d63d21303b57d9f27bcde27
Abdurrehman050 <abdurrehmanfsd07@gmail.com>
2024-07-11 19:42:34 +0500

    routes and cleaning of server.js

---
+++ routes/comments/comments.js
@@ -0,0 +1,50 @@
+const express = require("express");
+
+const commentRoutes = express.Router();
+
+//* POST/api/v1/comments
+commentRoutes.post("/", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Comment created",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+//* GET/api/v1/comments/:id
+commentRoutes.get("/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "single comment details",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+//* DELETE/api/v1/comments/:id
+commentRoutes.delete("/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Comment deleted",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+//* PUT/api/v1/comments/:id
+commentRoutes.put("/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Comment updated",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = commentRoutes;
---
+++ routes/posts/posts.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const postRoutes = express.Router();
+
+//* POST/api/v1/posts
+postRoutes.post("/", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Post created",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+//* GET/api/v1/posts
+postRoutes.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Posts list fetched",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+//* GET/api/v1/posts/:id
+postRoutes.get("/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Posts details",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+//* DELETE/api/v1/posts/:id
+postRoutes.delete("/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Post deleted",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+//* PUT/api/v1/posts/:id
+postRoutes.put("/:id", async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      user: "Post updated",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = postRoutes;
